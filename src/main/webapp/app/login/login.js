f80e394578e553c37c8ffb7003aac22880b011b3
stinaanita <stina-stud@live.dk>
2016-11-02 09:20:00 +0100

    provided a routeprovider to login

--- src/main/webapp/app/login/login.js
+++ src/main/webapp/app/login/login.js
@@ -1,7 +1,10 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+'use strict';
 
+angular.module('myApp.login', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/login', {
+    templateUrl: 'app/home/home.html'
+  });
+}]);
 
