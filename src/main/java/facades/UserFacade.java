2fcce036c401134e3522d565e84a31284dc75103
Diana <Diana@Dianas-MacBook-Pro.local>
2016-11-03 10:22:10 +0100

    no real changes made

--- src/main/java/facades/UserFacade.java
+++ src/main/java/facades/UserFacade.java
@@ -1,6 +1,5 @@
 package facades;
 
-import entity.Role;
 import security.IUserFacade;
 import entity.User;
 import java.util.List;
@@ -12,68 +11,61 @@ import security.IUser;
 import security.PasswordStorage;
 
 public class UserFacade implements IUserFacade {
-    
-    EntityManagerFactory emf;
-    
-    public UserFacade(EntityManagerFactory emf) {
-        this.emf = emf;        
-    }
-    
-    private EntityManager getEntityManager() {
-        return emf.createEntityManager();
-    }
-    
-    @Override
-    public IUser getUserByUserId(String id) {
-        EntityManager em = getEntityManager();
-        try {
-            return em.find(User.class, id);
-        } finally {
-            em.close();
-        }
-    }
 
-    /*
+  EntityManagerFactory emf;
+
+  
+  
+  public UserFacade(EntityManagerFactory emf) {
+    this.emf = emf;   
+  }
+
+  private EntityManager getEntityManager() {
+    return emf.createEntityManager();
+  }
+
+  @Override
+  public IUser getUserByUserId(String id) {
+    EntityManager em = getEntityManager();
+    try {
+      return em.find(User.class, id);
+    } finally {
+      em.close();
+    }
+  }
+
+  /*
   Return the Roles if users could be authenticated, otherwise null
-     */
-    @Override
-    public List<String> authenticateUser(String userName, String password) {
-        IUser user = getUserByUserId(userName);
-        try {
-            return user != null && PasswordStorage.verifyPassword(password, user.getPassword()) ? user.getRolesAsStrings() : null;
-        } catch (PasswordStorage.CannotPerformOperationException | PasswordStorage.InvalidHashException ex) {
-            Logger.getLogger(UserFacade.class.getName()).log(Level.SEVERE, null, ex);
-            return null;
-        }
+   */
+  @Override
+  public List<String> authenticateUser(String userName, String password) {
+    IUser user = getUserByUserId(userName);
+    try {
+      return user != null && PasswordStorage.verifyPassword(password, user.getPassword()) ? user.getRolesAsStrings() : null;
+    } catch (PasswordStorage.CannotPerformOperationException | PasswordStorage.InvalidHashException ex) {
+      Logger.getLogger(UserFacade.class.getName()).log(Level.SEVERE, null, ex);
+      return null;
     }
+  }
 
-    //Method for adding the new user
+  
+  //Method for adding the new user
     @Override
-    public IUser addUser(String userName, String password) {
+    public IUser addNewUser(String userName, String password) {
         User u = new User();
-        Role r = new Role("User");
+        
+        u.addUser(userName, password);
         
         EntityManager em = getEntityManager();
         try {
-            em.getTransaction().begin();
-            u.addUser(userName, password);
-            u.addRole(r);
-            em.persist(u);
-            em.getTransaction().commit();
-            return u;
-        } finally {
+        em.getTransaction().begin();
+        em.persist(u);
+        em.getTransaction().commit();
+        return u;
+        }
+        finally {
             em.close();
         }
     }
 
-    @Override
-    public IUser getPassword() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public IUser getUserName() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-    
 }
