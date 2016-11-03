2c3bc568138724eb49016468e6c2e25394b6aa65
stinaanita <stina-stud@live.dk>
2016-11-03 13:20:55 +0100

    nothing

--- src/main/webapp/app/signUp/signUp.js
+++ src/main/webapp/app/signUp/signUp.js
@@ -2,23 +2,8 @@
 
 angular.module('myApp.signUp', ['ngRoute'])
 
-        .config(['$routeProvider', function ($routeProvider) {
-                $routeProvider.when('/signUp', {
-                    templateUrl: 'app/signUp/signUp.html'
-                });
-            }])
-
-        .controller('signUpCtrl', function ($scope, $http) {
-            $scope.user = {};
-
-            $scope.signUp = function () {
-                var data = $.param({
-                    json: JSON.stringify({
-                    userName : $scope.user.userName,
-                    password: $scope.user.password})
-                });
-            };
-            $http.post('/api/signUp', data).success(function(data){
-                $scope.PostResponseDetail = data;
-            });
-        });
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/signUp', {
+    templateUrl: 'app/signUp/signUp.html'
+  });
+}]);
