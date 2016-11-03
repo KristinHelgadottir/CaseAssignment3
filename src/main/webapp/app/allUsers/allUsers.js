c6bda7b83522e68ab0d01bb95d316f00bf31dae9
Jarmo Hakkarainen <Jarmo Hakkarainen>
2016-11-03 18:16:12 +0100

    Made considerable changes to allUsers.html and js implementing most of the logic for showing all users. Small modifications to the draft of the calculator

--- src/main/webapp/app/allUsers/allUsers.js
+++ src/main/webapp/app/allUsers/allUsers.js
@@ -2,18 +2,32 @@
 
 angular.module('myApp.allUsers', ['ngRoute'])
 
-.config(['$routeProvider', function($routeProvider) 
-  {
-    $routeProvider.when('/allUsers', 
-    {
-      templateUrl: 'app/allUsers/allUsers.html',
-      controller: 'AllUsersCtrl',
-      controllerAs : 'ctrl'
-    });
-}])
+        .config(['$routeProvider', function ($routeProvider)
+            {
+                $routeProvider.when('/allUsers',
+                        {
+                            templateUrl: 'app/allUsers/allUsers.html',
+                            controller: 'AllUsersCtrl',
+                            controllerAs: 'ctrl'
+                        });
+            }])
 
-.controller('AllUsers', ["InfoFactory","InfoService",function(InfoFactory,InfoService) 
-{
-  this.msgFromFactory = InfoFactory.getInfo();
-  this.msgFromService = InfoService.getInfo();
-}]);
+        .controller('AllUsers', ["InfoFactory", "InfoService", function (InfoFactory, InfoService)
+            {
+                this.msgFromFactory = InfoFactory.getInfo();
+                this.msgFromService = InfoService.getInfo();
+            }])
+
+        .controller('getAllCtrl', function ($scope, $http) {
+            
+            $scope.users = [];
+                    
+                    $scope.getAll = function () {
+                $http({
+                    method: 'GET',
+                    url: '/api/getAll'
+                }).then(function (response) {
+                    $scope.users = response.data.records;
+                });
+            };
+        });
